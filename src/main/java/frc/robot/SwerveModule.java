90ad0fca2d93dcc2f39ee298d6c7d4c20b43a710
Celesteisscool <azzzlol5227@gmail.com>
2025-01-12 17:02:43 -0600

    raa raaa raaa raaaaaaaaaaaaaaaaa

---
+++ src/main/java/frc/robot/SwerveModule.java
@@ -0,0 +1,145 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.Encoder;
+
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkAbsoluteEncoder;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkRelativeEncoder;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+public class SwerveModule {
+  private static final double kWheelRadius = 0.0508;
+  private static final int kEncoderResolution = 4096;
+
+  private static final double kModuleMaxAngularVelocity = Drivetrain.kMaxAngularSpeed;
+  private static final double kModuleMaxAngularAcceleration =
+      2 * Math.PI; // radians per second squared
+
+  private final SparkMax m_driveMotor; 
+  private final RelativeEncoder m_driveEncoder;
+  private final SparkMax m_rotationMotor;
+  private final CANcoder m_rotationEncoder;
+
+  // Gains are for example purposes only - must be determined for your own robot!
+  private final PIDController m_drivePIDController = new PIDController(1, 0, 0);
+
+  // Gains are for example purposes only - must be determined for your own robot!
+  private final ProfiledPIDController m_turningPIDController =
+      new ProfiledPIDController(
+          1,
+          0,
+          0,
+          new TrapezoidProfile.Constraints(
+              kModuleMaxAngularVelocity, kModuleMaxAngularAcceleration));
+
+  // Gains are for example purposes only - must be determined for your own robot!
+  private final SimpleMotorFeedforward m_driveFeedforward = new SimpleMotorFeedforward(1, 3);
+  private final SimpleMotorFeedforward m_turnFeedforward = new SimpleMotorFeedforward(1, 0.5);
+
+  /**
+   * Constructs a SwerveModule with a drive motor, turning motor, drive encoder and turning encoder.
+   *
+   * @param driveMotorChannel CAN id for the drive motor.
+   * @param turningMotorChannel CAN id for the turning motor.
+   * @param turningEncoderChannel CAN id for the turning encoder.
+   */
+  public SwerveModule(
+      int driveMotorChannel,
+      int turningMotorChannel,
+      int turningEncoderChannel) {
+    m_driveMotor = new SparkMax(driveMotorChannel, MotorType.kBrushless);
+    m_rotationMotor = new SparkMax(turningMotorChannel, MotorType.kBrushless);
+    m_rotationEncoder = new CANcoder(turningEncoderChannel);
+    m_driveEncoder = m_driveMotor.getEncoder();
+    
+    // Limit the PID Controller's input range between -pi and pi and set the input
+    // to be continuous.
+    m_turningPIDController.enableContinuousInput(-Math.PI, Math.PI);
+  }
+
+  public double getDriveSpeedMetersPerSecond() {
+    double inchesPerMinute = m_driveEncoder.getVelocity() * 9.424777961;
+    double inchesPerSecond = inchesPerMinute / 60;
+    double metersPerSecond = inchesPerSecond * 0.0254;
+    return metersPerSecond;
+  }
+
+  /* Returns the distance traveled by the drive encoder in meters */
+  public double getDriveDistanceMeters() {
+    return (m_driveEncoder.getPosition() * 9.424777961) * 0.0254; // 9.4...  is the wheel circumference, 0.0254 is inches to meters
+  }
+
+  /* Returns the angle traveled by the turning encoder in radians */
+  public double getTurningEncoderPosition() {
+    return (m_rotationEncoder.getPosition().getValueAsDouble() * (2 *Math.PI)); // Returns the angle in radians
+  } 
+
+
+  /**
+   * Returns the current state of the module.
+   *
+   * @return The current state of the module.
+   */
+  public SwerveModuleState getState() {
+    return new SwerveModuleState(
+        getDriveSpeedMetersPerSecond(), new Rotation2d(getTurningEncoderPosition()));
+  }
+
+  /**
+   * Returns the current position of the module.
+   *
+   * @return The current position of the module.
+   */
+  public SwerveModulePosition getPosition() {
+    return new SwerveModulePosition(
+        getDriveDistanceMeters(), new Rotation2d(getTurningEncoderPosition()));
+  }
+
+  /**
+   * Sets the desired state for the module.
+   *
+   * @param desiredState Desired state with speed and angle.
+   */
+  public void setDesiredState(SwerveModuleState desiredState) {
+    var encoderRotation = new Rotation2d(getTurningEncoderPosition());
+
+    // Optimize the reference state to avoid spinning further than 90 degrees
+    desiredState.optimize(encoderRotation);
+
+    // Scale speed by cosine of angle error. This scales down movement perpendicular to the desired
+    // direction of travel that can occur when modules change directions. This results in smoother
+    // driving.
+    desiredState.cosineScale(encoderRotation);
+
+    // Calculate the drive output from the drive PID controller.
+
+    final double driveOutput =
+        m_drivePIDController.calculate(m_driveEncoder.getVelocity(), desiredState.speedMetersPerSecond);
+
+    final double driveFeedforward = m_driveFeedforward.calculate(desiredState.speedMetersPerSecond);
+
+    // Calculate the turning motor output from the turning PID controller.
+    final double turnOutput =
+        m_turningPIDController.calculate(
+            getTurningEncoderPosition(), desiredState.angle.getRadians());
+
+    final double turnFeedforward =
+        m_turnFeedforward.calculate(m_turningPIDController.getSetpoint().velocity);
+
+    m_driveMotor.setVoltage(driveOutput + driveFeedforward);
+    m_rotationMotor.setVoltage(turnOutput + turnFeedforward);
+  }
+}
