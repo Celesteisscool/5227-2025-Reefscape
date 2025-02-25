d2a7f33dec868f2e1bff599b745849dd1c923305
Celesteisscool <azzzlol5227@gmail.com>
2025-02-24 19:55:48 -0500

    make flipper work (dustpan)

--- src/main/java/frc/robot/Elevator.java
+++ src/main/java/frc/robot/Elevator.java
@@ -4,9 +4,12 @@ import com.revrobotics.spark.SparkMax;
 import edu.wpi.first.math.controller.ElevatorFeedforward;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard; 
 
 public class Elevator {
     private SparkMax elevatorMotor = new SparkMax(25, SparkMax.MotorType.kBrushless);
+    private SparkMax armMotor = new SparkMax(28, SparkMax.MotorType.kBrushless);
+    private SparkMax flipperMotor = new SparkMax(27, SparkMax.MotorType.kBrushless);
     private PIDController elevatorPIDController = new PIDController(1, 0, 0);
 
     private double ffS = 0; //PLEASE tune these once the elevator is assembled!!!
@@ -21,8 +24,15 @@ public class Elevator {
     double setPID;
     double setFeedForward;
 
+    public Elevator() {
+        elevatorMotor.getEncoder().setPosition(0);
+        armMotor.getEncoder().setPosition(0);
+    }
+
     // One liner elevator logic <3
     public void moveElevatorManual(double speed) { elevatorMotor.setVoltage(speed); }
+    public void moveArmManual(double speed) { armMotor.setVoltage(speed); }
+    public void moveFlipperManual(double speed) { flipperMotor.setVoltage(speed); }
     public void setPreset1() { preset1 = elevatorMotor.getEncoder().getPosition(); }
     public void setPreset2() { preset2 = elevatorMotor.getEncoder().getPosition(); }
 
@@ -37,6 +47,13 @@ public class Elevator {
         elevatorMotor.setVoltage(setPID + setFeedForward);
     }
 
+    public void reportPose() {
+        SmartDashboard.putNumber("Elevator Pose", elevatorMotor.getEncoder().getPosition());
+        SmartDashboard.putNumber("Arm Pose", armMotor.getEncoder().getPosition());
+        SmartDashboard.putNumber("Flipper Pose", flipperMotor.getEncoder().getPosition());
+    }
+
+
     public void elevatorLogic(Joystick elevatorController) {
         // Probably should switch to something more robust... Most likely something command based.
         // if (XboxController.getAButton()) { setPreset1(); }
@@ -47,11 +64,19 @@ public class Elevator {
         // else { moveElevatorManual(0); } // Prevents elevator from moving when not instructed.
         
         if (elevatorController.getRawButton(1)) {
-            double speed = elevatorController.getRawAxis(5) * -3;
-            moveElevatorManual(speed);
+            double elevatorSpeed = elevatorController.getRawAxis(5) * -4;
+            double armSpeed = elevatorController.getRawAxis(1) * -2;
+            double flipperSpeed = (elevatorController.getRawAxis(2) - elevatorController.getRawAxis(3) )* -2; 
+            moveElevatorManual(elevatorSpeed);
+            moveArmManual(armSpeed);
+            moveFlipperManual(flipperSpeed);
         } else {
             moveElevatorManual(0);
+            moveArmManual(0);
+            moveFlipperManual(0);
         }
+
+        reportPose();
 
     }
 }
