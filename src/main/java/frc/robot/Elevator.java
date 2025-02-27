87802ffcf276e61ba51dedcd673113982ce39673
Celesteisscool <azzzlol5227@gmail.com>
2025-02-26 19:27:22 -0500

    add some saftey to the elevator and flipper

--- src/main/java/frc/robot/Elevator.java
+++ src/main/java/frc/robot/Elevator.java
@@ -24,15 +24,77 @@ public class Elevator {
     double setPID;
     double setFeedForward;
 
+    double elevatorMaxHeight = 160;
+    double elevatorMinHeight = 0.5;
+    double elevatorSlowSpotHigh = elevatorMaxHeight-15;
+    double elevatorSlowSpotLow = elevatorMinHeight+15;
+
+    double flipperSetPose;
+    double flipperPoseMin = -12.5;
+    double flipperPoseMax = 0;
+
     public Elevator() {
         elevatorMotor.getEncoder().setPosition(0);
         armMotor.getEncoder().setPosition(0);
+        flipperMotor.getEncoder().setPosition(0);
     }
 
-    // One liner elevator logic <3
-    public void moveElevatorManual(double speed) { elevatorMotor.setVoltage(speed); }
+    public void moveElevatorManual(double speed) { 
+        speed = Math.min(1, Math.max(speed, -1)); // Caps it just incase
+        var elevatorPose = elevatorMotor.getEncoder().getPosition();
+        if ((elevatorPose >= elevatorMaxHeight) && (speed < 0)) {
+            speed = 0; // Stops if we are higher then max height
+        } 
+        if ((elevatorPose <= elevatorMinHeight) && (speed > 0)) {
+            speed = 0; // Stops if we are lower then max height
+        } 
+
+        if (elevatorPose > elevatorSlowSpotHigh) {
+            speed *= 0.5;
+        } else if (elevatorPose < elevatorSlowSpotLow) {
+            speed *= 0.5;
+        }
+        elevatorMotor.setVoltage(speed * -4);
+    }
+
+    public void moveFlipperPoses(Joystick elevatorController) {
+        var POV = elevatorController.getPOV();
+        
+        if (POV == 0) {
+            flipperSetPose = flipperPoseMax + 10;
+        } else if (POV == 180) {
+            flipperSetPose = flipperPoseMin - 10;
+        }
+
+        double flipperPose = flipperMotor.getEncoder().getPosition();
+        double speed = 0;
+        if (flipperSetPose > flipperPose )  {
+            speed = -1.0;
+        } else if (flipperSetPose < flipperPose) {
+            speed = 1.0;
+        }
+
+        moveFlipperManual(speed);
+
+    }
+
+
+    public void moveFlipperManual(double speed) { 
+        
+        var flipperPose = flipperMotor.getEncoder().getPosition();
+        if ((flipperPose < flipperPoseMin) && speed > 0) {
+            speed = 0;
+        }
+        if ((flipperPose > flipperPoseMax) && speed < 0) {
+            speed = 0;
+        }
+        
+        speed *= -2;
+        flipperMotor.setVoltage(speed); 
+        
+    }
+
     public void moveArmManual(double speed) { armMotor.setVoltage(speed); }
-    public void moveFlipperManual(double speed) { flipperMotor.setVoltage(speed); }
     public void setPreset1() { preset1 = elevatorMotor.getEncoder().getPosition(); }
     public void setPreset2() { preset2 = elevatorMotor.getEncoder().getPosition(); }
 
@@ -64,12 +126,13 @@ public class Elevator {
         // else { moveElevatorManual(0); } // Prevents elevator from moving when not instructed.
         
         if (elevatorController.getRawButton(1)) {
-            double elevatorSpeed = elevatorController.getRawAxis(5) * -4;
             double armSpeed = elevatorController.getRawAxis(1) * -2;
-            double flipperSpeed = (elevatorController.getRawAxis(2) - elevatorController.getRawAxis(3) )* -2; 
-            moveElevatorManual(elevatorSpeed);
+            double flipperSpeed = (elevatorController.getRawAxis(2) - elevatorController.getRawAxis(3)); 
+            moveElevatorManual(elevatorController.getRawAxis(5));
             moveArmManual(armSpeed);
             moveFlipperManual(flipperSpeed);
+        } else if (elevatorController.getRawButton(2)) {
+            moveFlipperPoses(elevatorController);
         } else {
             moveElevatorManual(0);
             moveArmManual(0);
